d42dc492b8af3bbecedfcba66368f10d5edadf98
lnwu <wind2729@gmail.com>
2019-06-08 00:53:54 +0800

    Li: setup eslint support

--- handler.ts
+++ handler.ts
@@ -3,9 +3,13 @@ import { APIGatewayEvent, Handler } from "aws-lambda"
 export const hello: Handler = async (event: APIGatewayEvent) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello Serverless in ${process.env.STAGE}`,
-      input: event,
-    }, null, 2),
-  };
+    body: JSON.stringify(
+      {
+        message: `Hello Serverless in ${process.env.STAGE}`,
+        input: event
+      },
+      null,
+      2
+    )
+  }
 }
